e979d3a36df8b2971890df88e102557cd118732f
Arthur Hernandez Perez <arthurhernandez@tecgraf.puc-rio.br>
2025-07-31 15:01:31 -0300

    Graphics/Renderer: Create shader class

--- src/graphics/renderer/Shader.cpp
+++ src/graphics/renderer/Shader.cpp
@@ -1,1 +1,76 @@
 #include "Shader.hpp"
+#include <iostream>
+#include <string>
+#include <fstream>
+
+namespace graphics
+{
+    namespace renderer
+    {
+        Shader::Shader(const char * shaderFilePath,GLenum shaderType)
+        : _shaderType{shaderType}, _shaderFilePath{shaderFilePath}
+        {
+            std::cout << "Shader Type: " << _shaderType << ", constructor called: " << shaderFilePath << std::endl;
+            auto result = loadFromFile();
+            if(!result)
+            {
+                throw std::runtime_error("Nao foi possivel abrir o arquivo");
+            }
+
+            _shaderSource = *result;
+            _shader = glCreateShader(shaderType);
+            glShaderSource(_shader,1,&_shaderSource,NULL);
+            if(!compileShader())
+            {
+                throw std::runtime_error("Error ao compilador o shader do tipo " +
+                                        shaderType + std::string(": \n") + _infoLog);
+            }
+        }
+
+        bool Shader::compileShader()
+        {
+            glCompileShader(_shader);
+            GLint sucess;
+            glGetShaderiv(_shader,GL_COMPILE_STATUS,&sucess);
+            if (!sucess)
+            {
+                glGetShaderInfoLog(_shader, sizeof(_infoLog), NULL, _infoLog);
+                glDeleteShader(_shader);
+                _shader = 0;
+                return false;
+            }
+            return true;
+        }
+
+        GLuint Shader::getShaderId() const
+        {
+            return _shader;
+        }
+
+        Shader::~Shader()
+        {
+            std::cout << "Shader Type: " << _shaderType << ", destructor called: " << _shaderFilePath << std::endl;
+            glDeleteShader(_shader);
+            _shader = 0;
+        }
+        std::optional<const char*> Shader::loadFromFile()
+        {
+            std::string line;
+            std::string content;
+            std::ifstream fileStream(_shaderFilePath,std::ios::in);
+            if (!fileStream.is_open()) 
+            {
+                std::cerr << "Could not read file " << _shaderFilePath << ". File does not exist." << std::endl;
+                return std::nullopt;
+            }
+            while (!fileStream.eof()) 
+            {
+                std::getline(fileStream, line);
+                content.append(line + "\n");
+            }
+            fileStream.close();
+
+            return content.c_str();
+        }
+    }
+}
--- src/graphics/renderer/Shader.hpp
+++ src/graphics/renderer/Shader.hpp
@@ -1,1 +1,26 @@
 #pragma once
+#include <glad/glad.h>
+#include <optional>
+namespace graphics
+{
+    namespace renderer
+    {
+        class Shader
+        {
+            public:
+                Shader(const char * shaderFilePath,GLenum shaderType);
+                GLuint getShaderId() const;
+                ~Shader();
+            private:
+                bool compileShader();
+                std::optional<const char*> loadFromFile();
+            private:
+                const GLchar* _shaderSource;
+                GLenum _shaderType;
+                const char * _shaderFilePath;
+                //OpenGL shader id
+                GLuint _shader;
+                GLchar _infoLog[1024];
+        };
+    } // namespace renderer
+} // namespace graphics
