601d912ed0f89db24088b3a29a544475100ca6f7
Arthur Hernandez Perez <arthurhernandez@tecgraf.puc-rio.br>
2025-07-31 16:12:38 -0300

    Graphics/Renderer: Create VBO class

--- src/graphics/renderer/VertexBuffer.cpp
+++ src/graphics/renderer/VertexBuffer.cpp
@@ -1,1 +1,49 @@
 #include "VertexBuffer.hpp"
+#include <iostream>
+namespace graphics
+{
+    namespace renderer
+    {
+        VertexBufferObject::VertexBufferObject()
+        {
+            generateBuffer();
+        }
+        
+        VertexBufferObject::~VertexBufferObject()
+        {
+        }
+
+        void VertexBufferObject::sendData(GLuint attributeLocation,GLint numberOfElements,GLenum dataType) const
+        {
+            bindBuffer();
+            glEnableVertexAttribArray(attributeLocation);
+            glVertexAttribPointer(
+                attributeLocation,numberOfElements,dataType,
+                GL_FALSE,0,(void*)0
+            );
+            std::cout << "Enviando os dados do VBO para o Vertex Shader" << '\n';
+        }
+
+        void VertexBufferObject::bindBuffer() const
+        {
+            glBindBuffer(GL_ARRAY_BUFFER,_vbo);
+        }
+
+        void VertexBufferObject::generateBuffer()
+        {
+            glGenBuffers(1,&_vbo);
+        }
+        void VertexBufferObject::receiveData(const float* data, size_t count, GLenum dataUsage)
+        {
+            bindBuffer();
+            glBufferData(GL_ARRAY_BUFFER,
+                    count,
+                    data,
+                    dataUsage);
+            std::cout << "Criando VBO e colocando os dados de positions no buffer" << '\n';
+
+        }
+    } // namespace renderer
+    
+} // namespace graphics
+
--- src/graphics/renderer/VertexBuffer.hpp
+++ src/graphics/renderer/VertexBuffer.hpp
@@ -1,1 +1,22 @@
 #pragma once
+#include <glad/glad.h>
+
+namespace graphics
+{
+    namespace renderer
+    {
+        class VertexBufferObject
+        {
+            public:
+                VertexBufferObject();
+                ~VertexBufferObject();
+                void receiveData(const float* data, size_t count, GLenum dataUsage);
+                void sendData(GLuint attribute,GLint numberOfElements,GLenum dataType) const;
+                void bindBuffer() const;
+            private:
+                void generateBuffer();
+            private:
+                GLuint _vbo;
+        };
+    }//namespace renderer  
+} // namespace graphics
