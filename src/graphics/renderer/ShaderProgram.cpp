5fd5fbfe70f2ba60cd435ddfd8c5895d9904846a
arthurhernandezp <arthurhernandezp@gmail.com>
2025-08-04 21:43:40 -0300

    Graphics/Renderer: Create Shader Program class

---
+++ src/graphics/renderer/ShaderProgram.cpp
@@ -0,0 +1,87 @@
+#include "ShaderProgram.hpp"
+#include "Shader.hpp"
+#include <iostream>
+#include <memory>                   //unique_ptr
+
+namespace graphics
+{
+    namespace renderer
+    {
+        ShaderProgram::ShaderProgram(const char *vertexShaderFilePath, const char *fragmentshaderFilePath)
+        {
+            std::cout << "Shader Program Constructor called" << '\n';
+            try{
+                _vs = std::make_unique<graphics::renderer::Shader>(vertexShaderFilePath,GL_VERTEX_SHADER);
+
+                _fs = std::make_unique<graphics::renderer::Shader>(fragmentshaderFilePath,GL_FRAGMENT_SHADER);
+
+                _program = glCreateProgram();
+                attachShaders();
+                linkShaders();
+            }
+            catch(const std::exception& e)
+            {
+                std::cerr << e.what() << '\n';
+            }
+        }
+
+        ShaderProgram::~ShaderProgram()
+        {
+            std::cout << "ShaderProgram destructor called" << '\n';
+            glDetachShader(_program,_vs->getShaderId());
+            glDetachShader(_program,_fs->getShaderId());
+        }
+
+        GLuint &ShaderProgram::getProgramId()
+        {
+            return _program;
+        }
+
+        void ShaderProgram::use()
+        {
+            glUseProgram(_program);
+        }
+
+        GLint ShaderProgram::getUniformVarPosition(const char * uniformVariable)
+        {
+            return glGetUniformLocation(_program, uniformVariable);
+        }
+
+        GLuint ShaderProgram::getAttribLocation(const char *vertexAttribute) const
+        {
+            return glGetAttribLocation(_program,vertexAttribute);
+        }
+
+        void ShaderProgram::sendUniformMat4(const char *uniformVariable,glm::mat4 matrix)
+        {
+            GLint uniformVarLoc = getUniformVarPosition(uniformVariable);
+            if (uniformVarLoc != -1){
+                glUniformMatrix4fv(uniformVarLoc, 1, GL_FALSE, glm::value_ptr(matrix));
+            } else{
+                std::cerr << "Uniform 'mvp' não encontrado!" << std::endl;
+            }
+        }
+
+        void ShaderProgram::sendUniformFloat(const char *uniformVariable, float vData)
+        {
+            GLint uniformVarLoc = getUniformVarPosition(uniformVariable);
+            if (uniformVarLoc != -1){
+                glUniform1f(uniformVarLoc,vData);
+            } else{
+                std::cerr << "Uniform 'mvp' não encontrado!" << std::endl;
+            }
+        }
+
+        void ShaderProgram::attachShaders() const
+        {
+            glAttachShader(_program,_vs->getShaderId());
+            glAttachShader(_program,_fs->getShaderId());
+        }
+
+        void ShaderProgram::linkShaders() const
+        {
+            glLinkProgram(_program);
+        }
+
+    } // namespace renderer
+} // namespace graphics
---
+++ src/graphics/renderer/ShaderProgram.hpp
@@ -0,0 +1,38 @@
+#pragma once
+#include "glad/glad.h"
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>     //glm::mat4 identity = glm::mat4(1.0f);
+#include <glm/gtc/type_ptr.hpp>             //glm::value_ptr
+
+#include <vector>
+#include <memory>
+namespace graphics
+{
+    namespace renderer
+    {
+        class Shader;
+
+        class ShaderProgram
+        {
+            public:
+                ShaderProgram(const char * vertexShaderFilePath,
+                const char * fragmentshaderFilePath);
+                ~ShaderProgram();
+                GLuint& getProgramId();
+
+                void use();
+                GLint getUniformVarPosition(const char * uniformVariable);
+                GLuint getAttribLocation(const char * vertexAttribute) const;
+                void sendUniformMat4(const char * uniformVariable, glm::mat4 matrix);
+                void sendUniformFloat(const char * uniformVariable, float vData);
+            private:
+                void attachShaders() const;
+                void linkShaders() const;
+            private:
+                GLuint _program;
+                std::unique_ptr<Shader> _vs;
+                std::unique_ptr<Shader> _fs;
+        };
+    } // namespace renderer
+} // namespace graphics
