25cab3a02f69653d3fa575255ed29edee45f2990
Arthur Hernandez <arthurhernandezp@gmail.com>
2025-08-06 16:06:04 -0300

    Graphics/Renderer: Create Vertex Array Object(VAO) class

--- src/graphics/renderer/VertexArray.cpp
+++ src/graphics/renderer/VertexArray.cpp
@@ -1,1 +1,39 @@
 #include "VertexArray.hpp"
+
+namespace graphics
+{
+    namespace renderer
+    {
+        VertexArrayObject::VertexArrayObject()
+        {
+            glGenVertexArrays(1, &_ID);
+        }
+
+        VertexArrayObject::~VertexArrayObject()
+        {
+            glDeleteVertexArrays(1, &_ID);
+        }
+
+        // Links a VBO to the VAO using a certain layout
+        void VertexArrayObject::LinkVBO(VertexBufferObject& VBO, GLuint layout)
+        {
+            VBO.bindBuffer();
+            glVertexAttribPointer(layout, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+            glEnableVertexAttribArray(layout);
+            VBO.unbindBuffer();
+        }
+
+        // Binds the VAO
+        void VertexArrayObject::bindBuffer()
+        {
+            glBindVertexArray(_ID);
+        }
+
+        // Unbinds the VAO
+        void VertexArrayObject::unbindBuffer()
+        {
+            glBindVertexArray(0);
+        }
+
+    } // namespace renderer
+} // namespace graphics
--- src/graphics/renderer/VertexArray.hpp
+++ src/graphics/renderer/VertexArray.hpp
@@ -1,1 +1,27 @@
 #pragma once
+#include "VertexBuffer.hpp"
+#include<glad/glad.h>
+namespace graphics
+{
+    namespace renderer
+    {
+        class VertexArrayObject
+            {
+            public:
+                // Constructor that generates a VAO ID
+                VertexArrayObject();
+                ~VertexArrayObject();
+                // Links a VBO to the VAO using a certain layout
+                void LinkVBO(VertexBufferObject& VBO, GLuint layout);
+                void bindBuffer();
+                void unbindBuffer();
+                GLuint getVAOId();
+            private:
+
+            private:
+                // ID reference for the Vertex Array Object
+                GLuint _ID;
+            };
+    } // namespace renderer
+
+} // namespace graphics
