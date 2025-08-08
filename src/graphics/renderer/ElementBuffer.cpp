2c6115e55429741b8d5f2866038e9af3f2281671
Arthur Hernandez <arthurhernandezp@gmail.com>
2025-08-07 21:28:05 -0300

    Graphics/Renderer: Create Element Buffer Object(EBO) class

---
+++ src/graphics/renderer/ElementBuffer.cpp
@@ -0,0 +1,33 @@
+#include "ElementBuffer.hpp"
+
+namespace graphics
+{
+    namespace renderer
+    {
+        ElementBufferObject::ElementBufferObject(GLuint *indices, GLsizeiptr size)
+        {
+            glGenBuffers(1, &_ID);
+            bindBuffer();
+            glBufferData(GL_ELEMENT_ARRAY_BUFFER, size, indices, GL_STATIC_DRAW);
+        }
+
+        ElementBufferObject::~ElementBufferObject()
+        {
+            glDeleteBuffers(1, &_ID);
+        }
+
+        void ElementBufferObject::bindBuffer()
+        {
+            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _ID);
+        }
+
+        void ElementBufferObject::unbindBuffer()
+        {
+            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+        }
+        GLuint ElementBufferObject::getEBOId()
+        {
+            return _ID;
+        }
+    } // namespace renderer
+} // namespace graphics
---
+++ src/graphics/renderer/ElementBuffer.hpp
@@ -0,0 +1,26 @@
+#pragma once
+#include<glad/glad.h>
+
+namespace graphics
+{
+    namespace renderer
+    {
+        class ElementBufferObject
+        {
+        public:
+            // Constructor that generates a Elements Buffer Object and links it to indices
+            ElementBufferObject(GLuint* indices, GLsizeiptr size);
+            ~ElementBufferObject();
+            // Binds the EBO
+            void bindBuffer();
+            // Unbinds the EBO
+            void unbindBuffer();
+            GLuint getEBOId();
+        private:
+
+        private:
+            // ID reference of Elements Buffer Object
+            GLuint _ID;
+        };
+    } // namespace renderer
+} // namespace grephics
