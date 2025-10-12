dcec7a7ccd1def8f9e0a93a35668f260e331ba72
Arthur Hernandez <arthurhernandezp@gmail.com>
2025-10-11 22:31:33 -0300

    Test/Math_t: Add unit tests for operator oveloading from Vector3D

--- tests/math_t/Vector3D_test.cpp
+++ tests/math_t/Vector3D_test.cpp
@@ -1,13 +1,22 @@
 #include <gtest/gtest.h>
 #include "math/Vector3D.hpp"
 
-TEST(Vector3DTest,ConstructorWithParams)
+TEST(Vector3DTest,DefaultConstructor)
 {
-    math::Vector3D vec(0.0f,0.0f,0.0f);
+    math::Vector3D vec;
 
     EXPECT_EQ(vec.x, 0.0f);
     EXPECT_EQ(vec.y, 0.0f);
     EXPECT_EQ(vec.z, 0.0f);
+}
+
+TEST(Vector3DTest,ConstructorWithParams)
+{
+    math::Vector3D vec(1.0f,1.0f,1.0f);
+
+    EXPECT_EQ(vec.x, 1.0f);
+    EXPECT_EQ(vec.y, 1.0f);
+    EXPECT_EQ(vec.z, 1.0f);
 }
 
 TEST(Vector3DTest,AccessVector3DMembers)
@@ -17,4 +26,58 @@ TEST(Vector3DTest,AccessVector3DMembers)
     EXPECT_EQ(vec[0], 1.0f);
     EXPECT_EQ(vec[1], 2.0f);
     EXPECT_EQ(vec[2], 3.0f);
+}
+
+TEST(Vector3DTest,AddVector3D)
+{
+    math::Vector3D vec1(1.0f,2.0f,3.0f);
+    math::Vector3D vec2(3.0f,3.0f,3.0f);
+    math::Vector3D vec3 = vec1 + vec2;
+    EXPECT_EQ(vec3.x, 4.0f);
+    EXPECT_EQ(vec3.y, 5.0f);
+    EXPECT_EQ(vec3.z, 6.0f);
+}
+
+TEST(Vector3DTest,SubVector3D)
+{
+    math::Vector3D vec1(1.0f,2.0f,3.0f);
+    math::Vector3D vec2(3.0f,3.0f,3.0f);
+    math::Vector3D vec3 = vec1 - vec2;
+
+    EXPECT_EQ(vec3.x, -2.0f);
+    EXPECT_EQ(vec3.y, -1.0f);
+    EXPECT_EQ(vec3.z, 0.0f);
+}
+
+TEST(Vector3DTest,AssignmentVector3D)
+{
+    math::Vector3D vec1(1.0f,2.0f,3.0f);
+    math::Vector3D vec2 = vec1;
+
+    EXPECT_EQ(vec2.x, 1.0f);
+    EXPECT_EQ(vec2.y, 2.0f);
+    EXPECT_EQ(vec2.z, 3.0f);
+}
+
+TEST(Vector3DTest,PlusEqualVector3D)
+{
+    math::Vector3D vec1(1.0f,2.0f,3.0f);
+    math::Vector3D vec2(3.0f,3.0f,3.0f);
+
+    vec2 += vec1;
+
+    EXPECT_EQ(vec2.x, 4.0f);
+    EXPECT_EQ(vec2.y, 5.0f);
+    EXPECT_EQ(vec2.z, 6.0f);
+}
+
+TEST(Vector3DTest,MinusEqualVector3D)
+{
+    math::Vector3D vec1(1.0f,2.0f,3.0f);
+    math::Vector3D vec2(3.0f,3.0f,3.0f);
+    vec1 -= vec2;
+
+    EXPECT_EQ(vec1.x, -2.0f);
+    EXPECT_EQ(vec1.y, -1.0f);
+    EXPECT_EQ(vec1.z, 0.0f);
 }
